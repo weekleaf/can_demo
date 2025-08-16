65bd3f537c8aac488c3f8e32655e5bc7d182246e
weekleaf <2375899670@qq.com>
2025-08-16 03:40:50 +0000

    delete some useless code

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,13 @@
-use socketcan::{CanSocket, CanFrame, CanDataFrame, Socket, EmbeddedFrame, StandardId, Frame};
-use std::time::Duration;
-use tokio::sync::Mutex;
 use futures_util::{SinkExt, StreamExt};
 use lazy_static::lazy_static;
 use pid::Pid;
+use socketcan::{CanDataFrame, CanFrame, CanSocket, EmbeddedFrame, Frame, Socket, StandardId};
+use std::time::Duration;
+use tokio::sync::Mutex;
 
-// fn main() {
-//     let socket = CanSocket::open("can0").expect("Failed to open CAN");
-
-//     let p_des: u16 = 0;
-//     let v_des: u16 = 0;
-//     let kp: u16 = 0;
-//     let kd: u16 = 0;
-//     let t_ff: u16 = 10; 
-
-//     let frame = CanDataFrame::new(
-//         StandardId::new(0x10).expect("CAN id is FKING"),
-//         &[
-//             (p_des >> 8) as u8,
-//             (p_des & 0xFF) as u8,
-//             (v_des & 0x0FF0) as u8, 
-//             ((v_des & 0x000F) | (kp & 0x0F00)) as u8,
-//             (kp & 0xFF) as u8,
-//             (kd & 0x0FF0) as u8,
-//             ((kd & 0x000F) | (t_ff & 0x0F00)) as u8,
-//             (t_ff & 0xFF) as u8
-//         ],
-//     )
-//     .expect("Failed to create CAN frame");
-
-//     let frame = CanFrame::Data(frame);
-
-//     loop {
-//         socket.write_frame(&frame).expect("Failed to send CAN frame");
-//     }
-    
-// }
-
-lazy_static! { static ref MOTOR1: Mutex<Motor> = Mutex::new(Motor::new()); }
+lazy_static! {
+    static ref MOTOR1: Mutex<Motor> = Mutex::new(Motor::new());
+}
 
 fn limit_min_max(x: &mut f32, min: f32, max: f32) {
     if *x <= min {
@@ -55,7 +25,7 @@ fn float_2_uint(mut x: f32, x_min: f32, x_max: f32, bits: u8) -> u16 {
 }
 
 fn uint_2_float(x: u16, x_min: f32, x_max: f32, bits: u8) -> f32 {
-    let span =  x_max - x_min;
+    let span = x_max - x_min;
     let data_norm = x as f32 / (((1 << bits) - 1) as f32);
     data_norm * span + x_min
 }
@@ -76,9 +46,9 @@ impl Motor {
     }
 
     fn handle_frame(&mut self, frame: &CanDataFrame) {
-        let mut read_data  = frame.data();
+        let mut read_data = frame.data();
         let read_pos_uint = ((read_data[1]) as u16) << 8 | (read_data[2] as u16);
-        let read_vel_uint = ((read_data[3] as u16)) << 4 | ((read_data[4] as u16)) >> 4;
+        let read_vel_uint = (read_data[3] as u16) << 4 | (read_data[4] as u16) >> 4;
         let read_t_uint = ((read_data[4] & 0xF) as u16) << 8 | (read_data[5] as u16);
 
         self.read_pos = uint_2_float(read_pos_uint, -3.141593, 3.141593, 16);
@@ -87,12 +57,10 @@ impl Motor {
 
         // println!("pos_uint : {}, vel_uint : {}, t_uint : {}", read_pos_uint, read_vel_uint, read_t_uint);
         // println!("pos : {}, vel : {}, t : {}", self.read_pos, self.read_vel, self.read_t);
-
-        
     }
 }
 
-async fn rotate_motor<T>(motor: &Motor, tx: &mut T/*, t_ff: f32*/)
+async fn rotate_motor<T>(motor: &Motor, tx: &mut T /*, t_ff: f32*/)
 where
     T: futures_util::sink::Sink<CanFrame, Error = socketcan::Error> + Unpin,
 {
@@ -103,47 +71,26 @@ where
 
     if pos_error > 3.141593 {
         pos_error -= 2.0 * 3.141593;
-    } else if  pos_error < -3.141593 {
+    } else if pos_error < -3.141593 {
         pos_error += 2.0 * 3.141593;
     }
 
-    let mut pid: Pid<f32> =  Pid::new(0.0, 5.0);
+    let mut pid: Pid<f32> = Pid::new(0.0, 5.0);
     pid.p(0.1, 100.0);
     pid.i(0.0, 100.0);
     pid.d(0.0, 100.0);
     let output = pid.next_control_output(pos_error);
-    
+
     let final_output = float_2_uint(output.output, -10.0, 10.0, 12);
 
     println!("===========================================================================");
     println!("output : {}, read_pos : {}", output.output, motor.read_pos);
     println!("final_output : {}", final_output);
 
-    // let p_des: f32 = 0.0;
-    // let v_des: f32 = 0.0;
-    // let kp: f32 = 0.0;
-    // let kd: f32 = 0.0;
-    // let t_ff: f32 = 10.0;
-
-    // let p_des_uint = float_2_uint(p_des, -3.141593, 3.141593, 16);
-    // let v_des_uint = float_2_uint(v_des, -50.0, 50.0, 12);
-    // let kp_uint = float_2_uint(kp, 0.0, 500.0, 12);
-    // let kd_uint = float_2_uint(kd, 0.0, 5.0, 12);
-    // let t_ff_uint = float_2_uint(t_ff, -10.0, 10.0, 12);
-
     let id: u16 = 0x10;
     let frame = CanDataFrame::new(
         StandardId::new(id).unwrap(),
         &[
-            // a problem! do not to use!!!
-            // (p_des_uint >> 8) as u8,
-            // (p_des_uint & 0xFF) as u8,
-            // (v_des_uint & 0x0FF0) as u8, 
-            // ((v_des_uint & 0x000F) | (kp_uint & 0x0F00)) as u8,
-            // (kp_uint & 0xFF) as u8,
-            // (kd_uint & 0x0FF0) as u8,
-            // ((kd_uint & 0x000F) | (t_ff_uint & 0x0F00)) as u8,
-            // (t_ff_uint & 0xFF) as u8
             0x00,
             0x00,
             0x00,
@@ -151,7 +98,7 @@ where
             0x00,
             0x00,
             (final_output >> 8) as u8,
-            (final_output & 0xFF) as u8
+            (final_output & 0xFF) as u8,
         ],
     )
     .unwrap();
